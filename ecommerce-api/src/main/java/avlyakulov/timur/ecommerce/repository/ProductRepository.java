8687602a41f45bc8e3e3cea91c664aeeb381414e
Timur <avlyakulovtimur@gmail.com>
2024-10-16 13:55:12 +0200

    add cross origin support

--- ecommerce-api/src/main/java/avlyakulov/timur/ecommerce/repository/ProductRepository.java
+++ ecommerce-api/src/main/java/avlyakulov/timur/ecommerce/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package avlyakulov.timur.ecommerce.repository;
 import avlyakulov.timur.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
